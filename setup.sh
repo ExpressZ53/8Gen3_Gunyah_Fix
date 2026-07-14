146eb0d363c869ab477d9b2fd6735eb963f8eb14
WTNLXTBL <FXD3D-J0001@outlook.com>
2026-07-14 11:03:02 +0800

    Fix

--- setup.sh
+++ setup.sh
@@ -3,15 +3,13 @@ set -euo pipefail
 
 MODULE_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 
-abk_require_env KERNEL_ROOT
-
-cd ${KERNEL_ROOT}/drivers/virt/gunyah
+cd ${KERNEL_ROOT}/common/drivers/virt/gunyah
 
 patch -p0 < ${MODULE_DIR}/patches/*.patch
 
 if [ $? == 0 ];then
-abk_log "Patched Successfully!"
+echo "Patched Successfully!"
 else
-abk_log "Patch Failed!"
+echo "Patch Failed!"
 exit 1
 fi
